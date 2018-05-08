17e52ff4262d3fad22d5c8f0d8ca87916742f7a7
Adrienne Yao <adrienne.yao@icloud.com>
2018-05-08 21:10:08 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,11 @@
 class Person
   #your code here
+attr_accessor :name, :age, :location, :user_name
+
+def initialize(user_name:, name:, age:, location:)
+  @user_name = user_name
+  @name = name
+  @location = location
+  @age = age
+end
 end
