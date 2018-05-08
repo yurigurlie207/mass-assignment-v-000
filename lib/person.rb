d7d429cc9b8f25ba12ebf5c91015afe801f16314
Adrienne Yao <adrienne.yao@icloud.com>
2018-05-08 21:20:56 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -5,7 +5,7 @@ attr_accessor :name, :birthday, :hair_color, :eye_color, :height,
 :wrist_size, :glove_size, :pant_length, :pant_width
 
 def initialize(attributes)
-  attributes.each { |key, value| self.send (("#{key}="), value)}
+  attributes.each { |key, value| self.send(("#{key}="), value)}
 end
 
 end
